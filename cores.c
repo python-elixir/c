f1e2a24eeee64154e56fc319d1f0b35068acd1b3
ulugbekpy <ulugbekpycpp@gmail.com>
2023-08-28 10:40:24 +0500

    strings initial part

--- cores.c
+++ cores.c
@@ -3,24 +3,8 @@
 
 int main() {
 
-  int myNumbers[] = {1,2,3,4,5,6,7}; 
-
-  printf("%i\n",myNumbers[3]);
-
-  myNumbers[5] = 9;
-  printf("%d",myNumbers[5]);
+  char greetings[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
+  printf("%s", greetings);
   
-int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
-
-int i, j;
-for (i = 0; i < 2; i++) {
-  printf("\n");
-  for (j = 0; j < 3; j++) {
-    printf("%d\t", matrix[i][j]);
-  }
-}
-
-char mychar = 'w';
-printf("%c",mychar);
   return 0;
 }
