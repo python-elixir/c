293e3aad2ccfa66734e1ef542877f58f3f636410
ulugbekpy <ulugbekpycpp@gmail.com>
2023-09-01 21:46:21 +0500

    functions first part

---
+++ functions.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+// void hello(char name[]){
+//     printf("Hello %s\n",name);
+// }
+
+// int adding(int x,int y){
+//     return x + y;
+// }
+
+// // This function calculates factorials
+// int factorial(int number){
+//     if (number > 0){
+//         return number*factorial(number - 1);
+//     }
+//     else{
+//         return 1;
+//     }
+// }
+
+int main(){
+    // char fullname[50];
+    // printf("%s\n","Men baxtliman");
+    // printf("%s","Ismingni kirit:...");
+    // fgets(fullname,sizeof(fullname),stdin);
+
+    // hello(fullname);
+    // int result = adding(2,3);
+    // printf("%i\n",result);
+    // int fact = factorial(10);
+    // printf("%d",fact);
+
+   
+    printf("%.2f\n",sqrt(225));
+    printf("%.1f\n",sqrt(64));
+
+    return 0;
+}
