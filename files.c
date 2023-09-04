23adfaf1751db53bc14db1a7a2923998edc43ab4
ulugbekpy <ulugbekpycpp@gmail.com>
2023-09-04 22:24:19 +0500

    almost finished

--- files.c
+++ files.c
@@ -2,23 +2,59 @@
 #include <string.h>
 #include <stdbool.h>
 
+struct Mystr {
+    int mynum;
+    char mychar;
+    bool mybool;
+};
+
 int main(){
     
-    FILE *fptr;
+    // FILE *myfile;
+    // FILE *myfolder;
 
-    // Create a file
-    fptr = fopen("start.py", "w");
+    // myfile = fopen("book.txt","w");
 
-    fprintf(fptr,"print(\"Error!\")");
+    // fprintf(myfile,"This is text file...");
 
-    // Close the file
-    fclose(fptr);
-    fptr = fopen("start.py", "a");
+    // fclose(myfile);
+    // myfile = fopen("book.txt","r");
+    // char mystring[100];
+    // fgets(mystring,sizeof(mystring),myfile);
 
-    fprintf(fptr,"\n errorr = \"just error\"");
-    fprintf(fptr,"\nprint(errorr)");
-    fclose(fptr);
+    // printf("\n%s\n",mystring);
 
+    // fclose(myfile);
+
+    // myfolder = fopen("go.py","r");
+
+    // if (myfolder == NULL) {
+    //     printf("This folder doesn't exist!");
+    // } else {
+    //     fclose(myfolder);
+    // }
+
+    struct Mystr s1;
+
+    s1.mybool = true;
+    printf("Enter your char and num:...");
+    scanf("%c %i\n",&s1.mychar,&s1.mynum);
+
+    printf("My char is\
+    %c\n",s1.mychar);
+    printf("My number is\
+    %i\n",s1.mynum);
+
+    switch (s1.mynum)
+    {
+    case 1:
+        printf("bir");
+        break;
+    
+    default:
+    printf("bir emas");
+        break;
+    }
 
     return 0;
 }
